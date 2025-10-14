e53eac084534fa9eeb0d0391008952ce24030b20
Arthur Rezende <arthurrezende165@gmail.com>
2025-10-14 15:48:50 -0300

    LogIn

---
+++ src/pages/login.jsx
@@ -0,0 +1,39 @@
+
+import {useState, useEffect} from 'react'
+import image from '../assets/image.png'
+import './login.css'
+
+const Login = () => {
+
+    const callapi = async (Email, Senha) => {
+    var resultado = await axios.post("https://localhost:17154/auth/login",{
+    Email: 'jakrh@hotmail.com',
+    Senha: '1934'
+    })
+
+    console.log(resultado)
+  }
+
+    return(
+        <>
+            <div className="login">
+                <div className="left-side">
+                </div>
+                <div className="right-side">
+                    <h1>Faça seu Login</h1>
+                    <form action="">
+                        <input type="text" className='Email' placeholder='Email' />
+                        <input type="password" className='Senha' placeholder='Senha' />
+                        <button className='Botao' onClick={() => {}}>LogIn</button>
+                        <div className="footer-form">
+                            <button className='CriarConta' onClick={() => {}}>Crie sua conta</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Login
